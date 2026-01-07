9de7c515084f0de6c70a8be37e66266664663fe5
Sahar <colab.saharaghakhani@gmail.com>
2026-01-07 14:52:16 +0330

    Update mae_extract_feature.py

--- scripts/mae_extract_feature.py
+++ scripts/mae_extract_feature.py
@@ -5,6 +5,7 @@ import argparse
 import tqdm
 import os.path as osp
 from PIL import Image
+import pandas as pd
 from transformers import VideoMAEModel, VideoMAEImageProcessor
 
 import sys
@@ -48,25 +49,35 @@ class VideoMAEFeatureReader(object):
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--anno_root', help='location of tsv files', required=True)
+    parser.add_argument('--annotation_root', help='location of tsv files', required=True)
     parser.add_argument('--video_root', help='location of tsv files', required=True)
+    parser.add_argument('--dataset_name', help='name of the dataset', default='IRSL-DB')
     parser.add_argument('--save_dir', help='where to save the output', required=True)
     parser.add_argument('--model_name', help='ViT model name', default='MCG-NJU/videomae-large')
     parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--device', help='device to use', default='cpu')
+    parser.add_argument('--device', help='device to use', default='cuda:0')
+    parser.add_argument('--window_size', type=int, default= 16)
     parser.add_argument('--overlap_size', type=int, default=8)
-    parser.add_argument('--mode', nargs='+', type=str)
+    # parser.add_argument('--mode', nargs='+', type=str)
     parser.add_argument('--nth_layer', type=int, default=-1)
     parser.add_argument('--cache_dir', help='cache dir for model', default=None)
+    parser.add_argument('--mode',nargs='+',choices=['train', 'test', 'dev'],default=['train', 'test', 'dev'],help='Modes to process: can be any combination of train, test, dev')
+
     return parser
 
 
-def get_iterator(args, mode):
+def get_iterator(args, mode, processed_files):
     batch_size = args.batch_size
+    ds_name = args.dataset_name
 
-    data = np.load(os.path.join(args.anno_root, f'{mode}_info.npy'), allow_pickle=True).item()
-    num = len(data) - 1
-    ds_name = osp.split(args.anno_root)[-1]
+    if ds_name =='IRSL-DB':
+        data = pd.read_csv(osp.join(args.annotation_root,f'{mode}.csv'))
+        num = len(data)
+    else:
+        data = np.load(os.path.join(args.annotation_root, f'{mode}_info.npy'), allow_pickle=True).item()
+        num = len(data) - 1
+
+    
 
     reader = VideoMAEFeatureReader(
         args.model_name, 
@@ -78,15 +89,18 @@ def get_iterator(args, mode):
     
     def iterate():
         for i in range(num):
-            fname = data[i]['folder']
             
             if ds_name == 'Phoenix14T' or ds_name == 'CSL-Daily':
+                fname = data[i]['folder']
+                if data[i]['fileid'] in processed_files:
+                    continue
+
                 image_list = get_img_list(ds_name, args.video_root, fname)
                 
-                if len(image_list) < 16:
-                    len_diff = 16 - len(image_list)
-                    image_list.extend([image_list[-1]] * (16 - len(image_list)))
-                image_list_chunks = sliding_window_for_list(image_list, window_size=16, overlap_size=args.overlap_size)
+                if len(image_list) < args.window_size:
+                    len_diff = args.window_size - len(image_list)
+                    image_list.extend([image_list[-1]] * (args.window_size - len(image_list)))
+                image_list_chunks = sliding_window_for_list(image_list, window_size=args.window_size, overlap_size=args.overlap_size)
                 
                 videos = []
                 for image_list in image_list_chunks:
@@ -100,17 +114,45 @@ def get_iterator(args, mode):
                     
                 yield np.concatenate(video_feats, axis=0), data[i]['fileid'], None
             
+            elif ds_name=='IRSL-DB':
+                fname = data['basename'][i]
+                if fname in processed_files:
+                    continue
+                videos = read_video(osp.join(args.video_root,f'{fname}.mp4'))
+                
+                if len(videos) > 0:
+                    if len(videos) < args.window_size:
+                        len_diff = args.window_size - len(videos)
+                        videos.extend([videos[-1]] * (args.window_size - len(videos)))
+                    
+                    videos = sliding_window_for_list(videos, window_size=args.window_size, overlap_size=args.overlap_size)
+                    
+                    video_feats = []
+                    for j in range(0, len(videos), batch_size):
+                        video_batch = videos[j:min(j + batch_size, len(videos))]
+                        feats = reader.get_feats(video_batch).cpu().numpy()
+                        video_feats.append(feats)
+                    
+                    yield np.concatenate(video_feats, axis=0), fname, None
+                
+                else:
+                    yield [], fname, None
+            
             else:
+                fname = data[i]['folder']
+                if data[i]['fileid'] in processed_files:
+                    continue
+
                 if ds_name == 'How2Sign':
                     start_time, end_time = data[i]['original_info']['START_REALIGNED'], data[i]['original_info']['END_REALIGNED']
                     videos = read_video(fname, start_time=start_time, end_time=end_time)
                     
                     if len(videos) > 0:
-                        if len(videos) < 16:
-                            len_diff = 16 - len(videos)
-                            videos.extend([videos[-1]] * (16 - len(videos)))
+                        if len(videos) < args.window_size:
+                            len_diff = args.window_size - len(videos)
+                            videos.extend([videos[-1]] * (args.window_size - len(videos)))
                         
-                        videos = sliding_window_for_list(videos, window_size=16, overlap_size=args.overlap_size)
+                        videos = sliding_window_for_list(videos, window_size=args.window_size, overlap_size=args.overlap_size)
                         
                         video_feats = []
                         for j in range(0, len(videos), batch_size):
@@ -129,24 +171,31 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    mode = ["dev", "test", "train"]
+    mode = args.mode
     for m in mode:
-        ds_name = osp.split(args.anno_root)[-1]
+        # ds_name = osp.split(args.anno_root)[-1]
+        ds_name = args.dataset_name
         fname = f'mae_feat_{ds_name}'
         os.makedirs(osp.join(args.save_dir, fname, m), exist_ok=True)
     
-        if ds_name == 'How2Sign':
+        if ds_name == 'How2Sign' or ds_name=='IRSL-DB':
             if m == 'dev': _m = 'val'
             else: _m = m
         elif ds_name == 'NIASL2021':
             if m == 'dev': _m = 'validation' 
         else:
             _m = m
-
-        generator, num = get_iterator(args, _m)
+        
+        log_file = osp.join(args.save_dir, fname,f"log_{m}.txt")
+        if osp.exists(log_file):
+            with open(log_file, 'r') as f:
+                processed_files = set(line.strip() for line in f)
+        else:
+            processed_files = set()
+        generator, num = get_iterator(args, _m, processed_files)
         iterator = generator()
 
-        for vit_feat in tqdm.tqdm(iterator, total=num):
+        for vit_feat in tqdm.tqdm(iterator, total=num, initial=len(processed_files)):
             feats, id, st = vit_feat
             save_path = osp.join(args.save_dir, fname, m)
             postfix = f'_overlap-{args.overlap_size}'
@@ -155,7 +204,8 @@ def main():
                 postfix = f'_{st}{postfix}'
             
             np.save(osp.join(save_path, f'{id}{postfix}.npy'), feats)
-
+            with open(log_file,'a') as f:
+                f.write(f"{id}\n")
 
 if __name__ == "__main__":
     main()
